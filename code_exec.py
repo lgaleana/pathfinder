6c170fa3d72725325c825a268d2696501f116353
Luis Galeana <lsgaleana@gmail.com>
2023-06-20 00:28:47 -0400

    Refactored code execution

--- code_exec.py
+++ code_exec.py
@@ -1,7 +1,12 @@
 import inspect
 import subprocess
 import sys
-from typing import Callable, List, Optional
+from contextlib import redirect_stdout
+from io import StringIO
+from typing import Any, Dict, Callable, List, Optional, Tuple
+
+from tasks import pip
+from utils.io import print_system, user_input
 
 
 def install_packages(packages: List[str]) -> None:
@@ -22,3 +27,50 @@ def get_function_by_exec(script: str) -> Optional[Callable]:
 def get_func_params(func: Callable):
     sig = inspect.signature(func)
     return sig.parameters
+
+
+def extract_imports_and_function(script: str) -> str:
+    keep_lines = []
+    for line in script.split("\n"):
+        if (
+            line.startswith("import")
+            or line.startswith("from")
+            or line.startswith("def")
+            or line.startswith("    ")  # Very naive
+        ):
+            keep_lines.append(line)
+    return "\n".join(keep_lines)
+
+
+def execute_code(
+    script: str,
+) -> Tuple[Optional[List[str]], str, Dict[str, Any], Any, str]:
+    code = extract_imports_and_function(script)
+    print_system(code)
+    if code:
+        func = get_function_by_exec(code)
+        packages = pip.get_packages(code)
+        print_system(f"Will install the following packages: {packages}")
+        if func:
+            params = get_func_params(func)
+            resolved_params = {}
+            print_system("Function inputs")
+            for name, param in params.items():
+                param_value = user_input(f"{name}: ")
+                if param.annotation is not param.empty:
+                    resolved_params[name] = param.annotation(param_value)
+                else:
+                    resolved_params[name] = param_value
+
+            if packages is not None:
+                install_packages(packages)
+            print_system("Running function...")
+            stdout = StringIO()
+            with redirect_stdout(stdout):
+                output = func(*resolved_params.values())
+            if stdout.getvalue():
+                print_system(stdout.getvalue())
+            print_system(output)
+
+            return packages, func.__name__, resolved_params, output, stdout.getvalue()
+    raise Exception("No function found to execute.")
--- main.py
+++ main.py
@@ -1,11 +1,9 @@
 import json
 import traceback
-from contextlib import redirect_stdout
-from io import StringIO
-from typing import Any, Dict, List, Optional, Tuple
+from typing import Dict, List
 
-from code_exec import get_func_params, get_function_by_exec, install_packages
-from tasks import chat, pip
+from code_exec import execute_code
+from tasks import chat
 from utils.io import print_system, user_input
 
 FOUR_K_TOKENS = 16000
@@ -47,7 +45,7 @@ def run(conversation: List[Dict[str, str]] = []) -> None:
                 continue
 
             try:
-                packages, func_name, inputs, output, stdout = run_code(code)
+                packages, func_name, inputs, output, stdout = execute_code(code)
                 if output is not None and len(output) >= FOUR_K_TOKENS:
                     system_message = (
                         "Function output is too long for the context window."
@@ -70,51 +68,6 @@ Function output: {output}"""
                     "content": system_message,
                 }
             )
-
-
-def run_code(code: str) -> Tuple[Optional[List[str]], str, Dict[str, Any], Any, str]:
-    code = extract_function(code)
-    print_system(code)
-    if code:
-        func = get_function_by_exec(code)
-        packages = pip.get_packages(code)
-        print_system(f"Will install the following packages: {packages}")
-        if func:
-            params = get_func_params(func)
-            resolved_params = {}
-            print_system("Function inputs")
-            for name, param in params.items():
-                param_value = user_input(f"{name}: ")
-                if param.annotation is not param.empty:
-                    resolved_params[name] = param.annotation(param_value)
-                else:
-                    resolved_params[name] = param_value
-
-            if packages is not None:
-                install_packages(packages)
-            print_system("Running function...")
-            stdout = StringIO()
-            with redirect_stdout(stdout):
-                output = func(*resolved_params.values())
-            if stdout.getvalue():
-                print_system(stdout.getvalue())
-            print_system(output)
-
-            return packages, func.__name__, resolved_params, output, stdout.getvalue()
-    raise Exception("No function found to execute.")
-
-
-def extract_function(script: str) -> str:
-    keep_lines = []
-    for line in script.split("\n"):
-        if (
-            line.startswith("import")
-            or line.startswith("from")
-            or line.startswith("def")
-            or line.startswith("    ")  # Very naive
-        ):
-            keep_lines.append(line)
-    return "\n".join(keep_lines)
 
 
 if __name__ == "__main__":
